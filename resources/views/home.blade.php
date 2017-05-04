7da7e2811344b56c61aa97e7565a82eeed39f03b
mtxfazdiz <mtxfazdiz@o2.pl>
2017-05-04 17:31:49 +0200

    new home page added (3 columns) col-md-10 coll-md-offset-1. text-center added to .container

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,99 @@
 - HOME
 @endsection
 
+
+
+
+
+
+
 @section('content')
-<div class="col-lg-6 col-lg-offset-2">
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2"> <!-- col8 start -->
+      
+
+
+      <div class="col-md-4"><!-- col4-01 start -->
+        <img class="img-circle" src="pic/av3.png" alt="Generic placeholder image"  height="100">
+              </br></br>
+              <h2>Dołącz do grona zadowolonych klientów</h2> 
+      </div><!-- col4-01 end -->
+      
+
+
+      <div class="col-md-4"><!-- col4-02 start -->
+        <img class="img-circle" src="pic/av1.png" alt="Generic placeholder image" width="140" height="140">
+              <h2></h2>
+              <p>Największą zaletą pożyczki w Certus jest szybki czas weryfikacji wniosków. Gotówkę można otrzymać nawet w ciągu 15 minut z dojazdem do klienta!</br>
+        <b>Pani Beata</b> z Braniewa</p>
+      </div><!-- col4-02 end -->
+        
+
+
+      <div class="col-md-4"><!-- col4-03 start -->
+        <img class="img-circle" src="pic/av2.jpg" alt="Generic placeholder image" width="140" height="140">
+              <h2></h2>
+              <p>Skorzystałem z Auto Pożyczki. Byłem zaskoczony, że moje auto w pełni zabezpieczyło pożyczkę, a ja nadal mogłem z niego korzystać!</br>
+        <b>Pan Krzysztof</b> z Węgorzewa</p>
+      </div><!-- col4-03 end -->
+    </div><!-- col8 end -->
+  </div>
+</div>
+
+
+
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@section('content2')
+
 
 
 
@@ -59,7 +150,7 @@
 
 
 
-</div>
+
 
 
 
